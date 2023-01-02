9a8cd9b307e0d5553652fa3756a0474af54c5c30
naveengulati0207 <121614208+naveengulati0207@users.noreply.github.com>
2023-01-02 15:03:54 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Kube',
+    version='1.1',
+    packages=[''],
+    url='https://github.com/LalitKockpit/kube.git',
+    license='',
+    author='hp',
+    author_email='naveen.gulati@kockpit.in',
+    description='test2'
+)
